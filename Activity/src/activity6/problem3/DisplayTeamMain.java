57161f1f57a683bcf5a80ea894b1a606bf027e7c
Microsoft <Microsoft@DESKTOP-2IOUUO9>
2021-08-15 19:53:58 +0530

    ..

--- Activity/src/activity6/problem3/DisplayTeamMain.java
+++ Activity/src/activity6/problem3/DisplayTeamMain.java
@@ -8,7 +8,6 @@ import java.util.TreeMap;
 
 public class DisplayTeamMain {
  public static void main(String[] args) {
-	 
 	 Scanner sc = new Scanner(System.in);
 	int n = sc.nextInt();
 	sc.nextLine();
